c62bdab545d9671e70c8b7dce7a86d30e939e294
Victoramz <ramirezvictorman@gmail.com>
2023-10-14 12:27:32 +0100

    Unenable rotation

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>bulbasur</Text>
+      <Image style={{width:200 , height:200 }} 
+      source={{uri: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png'}}/>
       <StatusBar style="auto" />
     </View>
   );
